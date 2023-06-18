7c55a1c85d188270bd8b5edc8be1ef3181220eaf
TheSameCat2 <thesamecat@proton.me>
2023-06-18 17:37:41 -0500

    Added some initial Nostr protocol stuff.

---
+++ NostrSharp/Events/Message.cs
@@ -0,0 +1,8 @@
+namespace NostrSharp.Events;
+
+public class Message
+{
+    public string? Type { get; set; }
+    public string? SubscriptionId { get; set; }
+    public string? Payload { get; set; }
+}
---
+++ NostrSharp/Events/Tag.cs
@@ -0,0 +1,8 @@
+namespace NostrSharp.Events;
+
+public struct Tag
+{
+    public string? Type;
+    public string? Payload;
+    public string? RecommendedRelay;
+}
