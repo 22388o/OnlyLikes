9121757fd8112537b5591f1e2e702e3c0e33de26
TheSameCat2 <thesamecat@proton.me>
2023-06-18 21:32:26 -0500

    Displays events

--- Relays/Relay.cs
+++ Relays/Relay.cs
@@ -1,8 +1,27 @@
-
+using Websocket.Client;
 
 namespace Relays;
 
 public class Relay {
-    public Relay() {
+    private readonly WebsocketClient _client;
+    public List<string> _queue = new();
+    public Relay(Uri uri) {
+        _client = new WebsocketClient(uri);
+        _client.MessageReceived.Subscribe(msg => {
+            Console.WriteLine(msg);
+            _queue.Add(msg.ToString());
+        });
+    }
+
+    public void Listen() {
+        _client.Start();
+    }
+
+    public List<string> GetEvents() {
+        return _queue;
+    }
+
+    public void Send(string msg) {
+        _client.Send(msg);
     }
 }
